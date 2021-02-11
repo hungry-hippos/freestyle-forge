b993c04b2933831076b6408e21d8f64e1d036167
Hungry Hippo <aduca96@gmail.com>
2021-02-11 14:46:41 -0800

    verse updates

--- src/appFiles/verseBank.js
+++ src/appFiles/verseBank.js
@@ -1,10 +1,13 @@
 const verseBank=[
     "Glock nineteen, my enemies slam the ground like a foam mattress,",
     "I got a double stack Big Mac and a rack of Wendy's tendies,",
-    "The good, the bad, Genghis Khan, gang of hoes riding mountain goats",
-    "Grilled cheese, turkey burgers, my ass makes deep fried ramen",
-    "Hoes ain't worth the coke, so I always give them salt to snort",
-    "Money moves. Refinanced two percent APR mortgage. Retirement king"
+    "The good, the bad, Genghis Khan, his gang of bros riding mountain goats",
+    "Money moves. Refinanced two percent APR mortgage. Retirement king",
+    "I mugged Charlie Brown, Peppermint Patty, Linus and Lucy",
+    "Chill vibes everybody, started out at the bottom and we still here",
+    "His palms are sweaty, knees weak, arms are heavy",
+    "I spit like an M16, let them all know they can have it",
+    "My flow is magic, can't catch it, it's classic, can't be contained"
 ]
 
 export default verseBank
